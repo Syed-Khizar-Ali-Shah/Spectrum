7974ef8271418e92fcf5ed8ca57a210c58cdb919
Syed-Khizar-Ali-Shah <115891972+Syed-Khizar-Ali-Shah@users.noreply.github.com>
2023-08-30 23:28:51 +0500

    updated!!

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const taskRoutes = require('./routes/task');
 
 app.use(cors(
     {
-        origin: ["https://spectrum-updated.verce"],
+        origin: ["https://spectrum-updated.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
